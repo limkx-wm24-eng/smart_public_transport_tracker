3913c98c0e3374ec98ce8bf883038c1cf0bc6681
limkx-wm24-eng <limkx-wm24@gmail.com>
2026-08-20 10:33:44 +0800

    Change MyApp to SmartTransportApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:smart_public_transport_tracker/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SmartTransportApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
